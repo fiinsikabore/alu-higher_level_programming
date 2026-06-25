19f100f858f8c1f28d5294413588dcd57346c434
root <f.kabore@alustudent.com>
2026-06-25 08:12:31 -0700

    task7

---
+++ python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+word_first_3 = word[:3]        # "Hol"
+word_last_2 = word[-2:]        # "on"
+middle_word = word[1:-1]       # sans 1er et dernier
+print("First 3 letters:", word_first_3)
+print("Last 2 letters:", word_last_2)
+print("Middle word:", middle_word)
