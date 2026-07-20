3026f9a5b56e2f93a30f5f8a554eb5fe6b39aaf8
root <f.kabore@alustudent.com>
2026-07-20 04:39:00 -0700

    task4

---
+++ python-classes/4-square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module that defines a class Square with size as a property."""
+
+
+class Square:
+    """Represent a square defined by a private, validated size attribute."""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): the size of the new square (default 0).
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): the new size of the square.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
