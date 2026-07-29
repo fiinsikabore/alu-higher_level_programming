9dd279adede56f74a6e7bf9b8d44e769ec5a9491
root <f.kabore@alustudent.com>
2026-07-29 07:45:47 -0700

    task7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""Script that adds command-line arguments to a list and saves it to JSON."""
+"""Script that adds command line arguments to a JSON file list."""
 import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    my_list = []
 
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, "add_item.json")
+if __name__ == "__main__":
+    filename = "add_item.json"
+
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(sys.argv[1:])
+
+    save_to_json_file(my_list, filename)
