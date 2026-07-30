eb575bf95f29dc40c4d34550ba4d6958e34ca8bb
root <f.kabore@alustudent.com>
2026-07-30 11:27:11 -0700

    task 100 101 102

---
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Lists all City objects from the database using the state relationship."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            username, password, db_name
+        ),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).order_by(City.id.asc()).all()
+
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    session.close()
