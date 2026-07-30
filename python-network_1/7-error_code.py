fad157408fc5cad7aae533058261dabb056432b3
root <f.kabore@alustudent.com>
2026-07-30 02:40:57 -0700

    update again

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module that takes in a URL, sends a request to it, and displays
+"""
+Script that takes in a URL, sends a request to the URL, and displays
 the body of the response. If the HTTP status code is greater than or
-equal to 400, it displays: Error code: <status code> instead.
+equal to 400, it prints the error code.
 """
 import requests
 import sys
@@ -10,6 +11,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
+
     if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
