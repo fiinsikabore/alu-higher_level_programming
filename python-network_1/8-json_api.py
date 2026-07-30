499b32706427c0dd9c81ed171e27e10f5cced02b
root <f.kabore@alustudent.com>
2026-07-30 02:36:46 -0700

    update

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Module that takes in a letter and sends a POST request to
-http://0.0.0.0:5000/search_user with the letter as a parameter,
-then displays the result based on the JSON response received.
+"""
+Script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 """
 import requests
 import sys
@@ -9,18 +9,22 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        letter = sys.argv[1]
+        q = sys.argv[1]
     else:
-        letter = ""
+        q = ""
 
     url = "http://0.0.0.0:5000/search_user"
-    response = requests.post(url, data={"q": letter})
+    data = {'q': q}
+
     try:
-        result = response.json()
-    except ValueError:
-        print("Not a valid JSON")
-    else:
-        if not result:
+        response = requests.post(url, data=data)
+        user_json = response.json()
+
+        if user_json == {}:
             print("No result")
         else:
-            print("[{}] {}".format(result.get("id"), result.get("name")))
+            user_id = user_json.get("id")
+            user_name = user_json.get("name")
+            print("[{}] {}".format(user_id, user_name))
+    except ValueError:
+        print("Not a valid JSON")
