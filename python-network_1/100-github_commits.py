0bf4bc8cbb09725b28080b9520452843ddb978b8
root <f.kabore@alustudent.com>
2026-07-30 05:30:32 -0700

    Fix import and indexing for 100-github_commits.py

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """
-Script that fetches the 10 most recent commits of a repository
-from a specific owner using the GitHub API.
+Script that lists 10 commits from most recent to oldest of a given repository.
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
 
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        owner_name, repo_name
-    )
+    r = requests.get(url)
+    commits = r.json()
 
-    response = requests.get(url)
     try:
-        commits = response.json()
-        for commit in commits[:10]:
-            sha = commit.get('sha')
-            author_name = commit.get('commit').get('author').get('name')
-            print("{}: {}".format(sha, author_name))
+        for i in range(10):
+            sha = commits[i].get('sha')
+            author = commits[i].get('commit').get('author').get('name')
+            print("{}: {}".format(sha, author))
     except IndexError:
         pass
