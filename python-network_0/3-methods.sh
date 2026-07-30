6ee2d81b9c7974c7f58548ccfeb6aa5af6c01702
root <f.kabore@alustudent.com>
2026-07-30 07:28:39 -0700

    Fix executable permissions for Task 3

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays all HTTP methods accepted by the target server
-curl -sI "$1" | grep -i "Allow:" | cut -d' ' -f2-
+# Displays all HTTP methods the server will accept
+curl -siX OPTIONS "$1" | grep -i "^Allow:" | cut -d ' ' -f2-
