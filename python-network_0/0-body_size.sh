1076cd7d68ce15affd71c6f3c6bec85164516f1e
root <f.kabore@alustudent.com>
2026-07-30 07:38:36 -0700

    Update 0-body_size.sh to use size_download

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the size of the response body in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+# Displays the size of the body of the response in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
