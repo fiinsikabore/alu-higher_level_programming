1db4386c0de44a304bf1f789bda2dee06f7d1fd1
root <f.kabore@alustudent.com>
2026-07-30 02:44:16 -0700

    updateee

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Module that takes in a URL and an email address, sends a POST
-request to the URL with the email as a parameter, and displays the
-body of the response.
+"""
+Script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and displays the body
+of the response.
 """
 import requests
 import sys
@@ -10,5 +11,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
-    response = requests.post(url, data={"email": email})
+    payload = {'email': email}
+
+    response = requests.post(url, data=payload)
     print(response.text)
