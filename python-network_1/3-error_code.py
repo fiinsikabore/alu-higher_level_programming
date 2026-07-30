b5867ca86b2b7fd9e478c47aed999ecb4330f9e1
root <f.kabore@alustudent.com>
2026-07-30 04:43:04 -0700

    more update

--- python-network_1/3-error_code.py
+++ python-network_1/3-error_code.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Module that takes in a URL, sends a request to the URL,
-and displays the body of the response (decoded in utf-8).
-Handles HTTPError exceptions.
+Script qui prend une URL en argument, envoie une requête et affiche
+le corps de la réponse décodé en UTF-8, tout en gérant les HTTPError.
 """
 import sys
 import urllib.error
@@ -11,9 +10,10 @@ import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
+    
+    req = urllib.request.Request(url)
     try:
-        with urllib.request.urlopen(url) as response:
-            body = response.read().decode('utf-8')
-            print(body)
+        with urllib.request.urlopen(req) as response:
+            print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
