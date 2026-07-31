fb6264edd325f65d682eb63e1c096717dc0eec4a
root <f.kabore@alustudent.com>
2026-07-31 07:09:30 -0700

    Task 4: Send custom cURL header

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with custom header x-HolbertonSchool-User-Id: 98
-curl -sH "x-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a custom header
+curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
