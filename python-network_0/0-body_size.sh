83e9748f36cd129564af54fe7976f22e4926f267
root <f.kabore@alustudent.com>
2026-07-31 05:17:58 -0700

    Task 0: cURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# Displays the size of the body of the response in bytes
-curl -s -o /dev/null -w "%{size_download}\n" "$1"
+# Sends a request to a URL and displays the size of the body of the response in bytes
+curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
+EOF
