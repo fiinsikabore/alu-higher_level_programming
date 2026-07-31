f94f16b631f40e974580080a804475e41d06ea44
root <f.kabore@alustudent.com>
2026-07-31 06:52:03 -0700

    Fix 0-body_size.sh line count to exactly 3 lines

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the body of the response in bytes
-curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
-EOF
+# Displays the size of the body of the response in bytes
+curl -s -o /dev/null -w "%{size_download}\n" "$1"
